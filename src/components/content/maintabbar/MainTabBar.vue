62037de8d697319ffdb8d7a7ce68f54d9a000763
wing-sylvia <1067998134@qq.com>
2021-04-03 21:16:37 +0800

    tabbar

---
+++ src/components/content/maintabbar/MainTabBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <tab-bar>
+      <tab-bar-item path="/home" activeColor="blue">
+        <template v-slot:item-icon>
+          <img  src="~assets/image/tabbar/home.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img  src="~assets/image/tabbar/home_active.png" alt="">
+        </template>
+        <template v-slot:item-text> 
+          <div>首页</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <template v-slot:item-icon>
+          <img  src="~assets/image/tabbar/categories.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img  src="~assets/image/tabbar/categories_active.png" alt="">
+        </template>
+        <template v-slot:item-text> 
+          <div>分类</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="cart">
+        <template v-slot:item-icon>
+          <img  src="~assets/image/tabbar/shopcart.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img  src="~assets/image/tabbar/shopcart_active.png" alt="">
+        </template>
+        <template v-slot:item-text> 
+          <div>购物车</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="profile">
+        <template v-slot:item-icon>
+          <img  src="~assets/image/tabbar/profile.png" alt="">
+        </template>
+        <template v-slot:item-icon-active>
+          <img  src="~assets/image/tabbar/profile_active.png" alt="">
+        </template>
+        <template v-slot:item-text> 
+          <div>我的</div>
+        </template>
+      </tab-bar-item>
+      
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar.vue'
+import TabBarItem from 'components/common/tabbar/TabBarItem.vue';
+export default {
+  components:{
+    TabBar,
+    TabBarItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = () => import("@/views/home/Home.vue")
+const Category = () => import("../views/category/Category.vue")
+const Cart = () => import("../views/cart/Cart.vue")
+const Profile = () => import("../views/profile/Profile.vue")
+
+const routes = [
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/home',
+    component: Home
+  },
+  {
+    path:'/cart',
+    component: Cart
+  },
+  {
+    path:'/category',
+    component: Category
+  },
+  {
+    path:'/profile',
+    component: Profile
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  mode:history
+})
+
+export default router
